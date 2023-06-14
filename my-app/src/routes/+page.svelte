0fe1e5c25fe87100575b288ea5497ddfd188513b
LimShuonle <srxmax161@gmail.com>
2023-06-14 17:22:40 +0800

    creating sign up page?

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button>
+    <div class="btn">
+        <a href="/sign-up">Sign up</a>
+    </div>
+</button>
---
+++ my-app/src/routes/sign-up/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+    // import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+    // import { goto } from '$app/navigation';
+
+    let formErrors = {};
+    async function createUser(){
+
+    }
+  </script>
+  
+  <h1 class="text-center text-xl">Create an Account to Post a Job</h1>
+  <div class="text-center">
+      <a class="link-hover italic text-xs" href="/login">Already have an account? Click here to login instead.</a>
+  </div>
+  <div class="flex justify-center items-center mt-8">
+      <form on:submit={createUser} class="w-1/3">
+          <div class="form-control w-full">
+              <label class="label" for="username">
+                  <span class="label-text">Username</span>
+              </label>
+              <input type="text" 
+                     name="username" 
+                     placeholder="example" 
+                     class="input input-bordered w-full" />
+              {#if 'username' in formErrors}
+              <label class="label" for="username">
+                  <span class="label-text-alt text-red-500">{formErrors['username'].message}</span>
+              </label>
+              {/if}
+          </div>
+  
+          <div class="form-control w-full">
+              <label class="label" for="email">
+                  <span class="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="example@example.com" class="input input-bordered w-full" required />
+              {#if 'email' in formErrors}
+              <label class="label" for="email">
+                  <span class="label-text-alt text-red-500">{formErrors['email'].message}</span>
+              </label>
+              {/if}
+          </div>
+  
+          <div class="form-control w-full">
+              <label class="label" for="password">
+                  <span class="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="" class="input input-bordered w-full" required />
+              {#if 'password' in formErrors}
+              <label class="label" for="password">
+                  <span class="label-text-alt text-red-500">{formErrors['password'].message}</span>
+              </label>
+              {/if}
+          </div>
+  
+          <div class="form-control w-full">
+              <label class="label" for="password">
+                  <span class="label-text">Confirm Password</span>
+              </label>
+              <input type="password" name="password-confirmation" placeholder="" class="input input-bordered w-full" required />
+              {#if 'password' in formErrors}
+              <label class="label" for="password">
+                  <span class="label-text-alt text-red-500">{formErrors['password'].message}</span>
+              </label>
+              {/if}
+          </div>
+  
+          <div class="form-control w-full mt-4">
+              <button class="btn btn-md">Create an Account</button>
+          </div>
+      </form>
+  </div>
+  
