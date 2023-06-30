50ca643861812a2d35d56650ac3fb84cce0ca9e2
LimShuonle <srxmax161@gmail.com>
2023-06-30 14:46:44 +0800

    updated everything and ready to deploy

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -1,26 +1,39 @@
 <script>
-	import { logOut, loggedIn } from "../utils/auth";
+	import { logOut, isLogged } from "../utils/auth.js";
+    import { goto } from '$app/navigation';
+    import "../../src/app.css";
+    
+    function uploadImage(){
+        goto ('/upload/new')
+        }
+    
+    function goToHomePage(){
+        goto ('/')
+    }
+
+    function goToSignUp(){
+        goto ('/sign-up')
+    }
+
+    function goToLogIn(){
+        goto ('/login')
+    }
 
 </script>
 
-<nav class="flex justify-between mt-5 px-3">
-    <div>
-        <button class="btn">
-            <a href="/">NEXT Academy Bootcamp Project - P2P EComm Full-Stack</a>
-        </button>
-    </div>
-    <div>
-        {#if $loggedIn == false}
+    <nav class="flex justify-between pt-5 pb-5 px-3 bg-lena-hair">
         <div>
-            </div>
-            <button class= "btn">
-                <a href='/sign-up'>Sign Up</a>
-            </button>
-        {:else}
-        <button class="btn">
-            <a href="">Picutres</a>
-        </button>
-        <button class="btn" on:click={logOut}>Log Out</button>
-        {/if}
+            <button class="btn bg-lena-uniform text-lena-hair" on:click={goToHomePage}>WaifuVerse</button>
         </div>
-</nav>
+        <div>
+            {#if $isLogged == false}
+            <div>
+                </div>
+                <button class="btn bg-lena-uniform text-lena-hair" on:click={goToSignUp}>Sign Up</button>
+                <button class="btn bg-lena-uniform text-lena-hair" on:click={goToLogIn}>Log In</button>
+            {:else}
+            <button class="btn bg-lena-uniform text-lena-hair" on:click={uploadImage}>Upload</button>
+            <button class="btn bg-lena-uniform text-lena-hair" on:click={logOut}>Log Out</button>
+            {/if}
+        </div>
+    </nav>
--- my-app/src/utils/auth.js
+++ my-app/src/utils/auth.js
@@ -1,70 +1,104 @@
-import { writable } from "svelte/store";
-export const loggedIn = writable(false)
-import { goto } from "$app/navigation";
-import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
+import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+import { writable } from 'svelte/store';
 
-const emptyAuth ={
-    "token": "",
-    "userId":""
+export const isLogged = writable(false);
+
+const emptyAuth = {
+    "accessToken": ""
 }
 
-export function logOut(){
-    localStorage.setItemI("auth",JSON.stringify(emptyAuth));
-    loggedIn.set(false)
-    goto('/')
-    return true
+isLogged.subscribe(value => {
+  console.log('Store value:', value);
+});
+
+export function logOut() {
+    localStorage.setItem("auth", JSON.stringify(emptyAuth));
+    isLogged.set(false);
+    return true;  
 }
 
-export function getUserId(){
-    const auth = localStorage.getItem("auth")
-    if (auth){
-        return JSON.parse(auth)["userId"]
-    }
-    return null
-}
-
-export function getTokenFromLocalStorage(){
-    const auth = localStorage.getItem("auth")
+export function getUserID() {
+    const auth = localStorage.getItem("auth");
     if (auth) {
-        return JSON.parse(auth)["token"]
+        return JSON.parse(auth)['userId'];
     }
-    return null
+    return null;
 }
 
-export async function authenticateUser(username, password) {
-    const resp = await fetch(
-      PUBLIC_BACKEND_BASE_URL + '/users/auth-with-password',
-      {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          identity: username,
-          password
-        })
-      }
-    );
+export function getTokenFromLocalStorage() {
+  const auth = localStorage.getItem("auth");
+  if (auth) {
+      return JSON.parse(auth)['accessToken'];
+  }
+  return null;
+}
 
-    const res = await resp.json();
+export async function isLoggedIn() {
+    if (!(getTokenFromLocalStorage())) {
+      isLogged.set(false)
+    }
+    else
+    isLogged.set(true)
+  }
 
-    if (resp.status == 200) {
-      localStorage.setItem("auth", JSON.stringify({
-        "token": res.token,
-        "userId": res.record.id
-      }));
-      
-      loggedIn.set(true)
-      
-      return {
+export async function authenticateUser(email, password) {
+  const resp = await fetch(
+    PUBLIC_BACKEND_BASE_URL + '/auth',
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }
+  );
+  const res = await resp.json();
+  if (resp.status === 200) {
+    localStorage.setItem("auth", JSON.stringify({
+        'accessToken': res.accessToken
+
+    }));
+    isLogged.set(true);
+    return {
         success: true,
         res: res
-      }
     }
-  
-    return {
-      success: false,
-      res: res
-    }
+  }
+  return {
+    success: false,
+    res: res
+  }
+}
+
+export async function imageCheckout(evt){
+  evt.preventDefault()
+
+  const price = evt.target.dataset.price;
+  const title = evt.target.dataset.title;
+  const url = evt.target.dataset.url;
+
+  const imageData = {
+    title: title,
+    price: price,
+    image: url,
+  }
+
+  const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/checkout',{
+    method: 'POST',
+    mode : 'cors',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(imageData)
+  });
+  if (resp.status == 200){
+    const res = await resp.json()
+    window.location.replace(res)
+  } else {
+    alert('Failed to checkout')
+  }
 }
